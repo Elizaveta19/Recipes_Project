2b6ea1c8ae1e9d3d34a46b8fb36be9c51a40ff7b
Elizaveta19 <elizaveta-1912@yandex.ru>
2015-11-01 00:10:21 +0200

    Drawer_3
    Еще ничего нет. только начала делать фрагменты.

---
+++ Recipes/app/src/main/java/com/acer/recipes/fragments/SearchRecipesFragment.java
@@ -0,0 +1,33 @@
+package com.acer.recipes.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.acer.recipes.R;
+
+public class SearchRecipesFragment extends Fragment {
+
+    private static final int LAYOUT = R.layout.activity_search_recipes;
+    private View view;
+
+    public static SearchRecipesFragment getFragment()
+    {
+        Bundle args = new Bundle();
+        SearchRecipesFragment fragment = new SearchRecipesFragment();
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = inflater.inflate(LAYOUT, container, false);
+        return view;
+    }
+}
