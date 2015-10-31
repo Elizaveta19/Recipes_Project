41c82d5c7192c4706a7238c9a017cf2f00a5207a
Elizaveta19 <elizaveta-1912@yandex.ru>
2015-10-31 22:18:20 +0200

    Drawer_2
    Пару item с иконками

--- Recipes/app/src/main/java/com/acer/recipes/MainActivity.java
+++ Recipes/app/src/main/java/com/acer/recipes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.acer.recipes;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,12 +9,21 @@ import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+    private DrawerLayout drawerLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppDefault);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
+        initNavigationView();
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     @Override
