85ab1fa53ae6e2e5215e6552f2c5cf8c67bd1a59
Elizaveta19 <elizaveta-1912@yandex.ru>
2015-11-08 00:19:25 +0200

    Drawer_7
    Парсит заголовок с сайта. Граница блока внизу

--- Recipes/app/src/main/java/com/acer/recipes/RecipesResult.java
+++ Recipes/app/src/main/java/com/acer/recipes/RecipesResult.java
@@ -2,13 +2,21 @@ package com.acer.recipes;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
 
 public class RecipesResult extends Activity{
     private static final int LAYOUT = R.layout.activity_recipes_result;
@@ -16,5 +24,42 @@ public class RecipesResult extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes_result);
+
+        MyTask mt = new MyTask();
+        mt.execute();
+    }
+
+
+
+    class MyTask extends AsyncTask<Void, Void, Void>
+    {
+
+        String title;
+        @Override
+        protected Void doInBackground(Void... params) {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect("http://harrix.org").get();
+            }
+            catch (IOException e) {
+                Log.v("MyConnect", "Connect error :c");
+                e.printStackTrace();
+            }
+
+            if(doc != null)
+                title = doc.title();
+            else
+                title = "Ошибка";
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result)
+        {
+            TextView textView = (TextView) findViewById(R.id.arr_name);
+            super.onPostExecute(result);
+            textView.setText(title);
+        }
     }
 }
--- Recipes/app/src/main/java/com/acer/recipes/fragments/SearchRecipesFragment.java
+++ Recipes/app/src/main/java/com/acer/recipes/fragments/SearchRecipesFragment.java
@@ -1,16 +1,25 @@
 package com.acer.recipes.fragments;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.acer.recipes.R;
 import com.acer.recipes.RecipesResult;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
 
 public class SearchRecipesFragment extends Fragment implements View.OnClickListener{
 
