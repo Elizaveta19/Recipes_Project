e775dc318107479e4fa9a390f6324ff783a63e09
Elizaveta19 <elizaveta-1912@yandex.ru>
2015-11-23 19:15:13 +0300

    Сервер_2
    Просто подключается

---
+++ Server/Backend/src/ServerMain.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class ServerMain extends Thread {
+
+		long n;
+		String HostName;
+		Socket socket;
+		BufferedReader in;
+		PrintWriter out;
+
+		public ServerMain(Socket s, String HostName, long n) throws IOException {
+		    this.n = n; //эта переменная получает по параметру № климента, который подключился
+		    this.HostName = HostName;  //эта переменная получает по параметру имя климента, который подключился
+		    this.socket = s;
+		    //в этупеременную будутт поступать данные
+		    this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		    //эта переменная на вывод данных клиенту
+		    this.out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+		    start();
+		}
+
+		public void run() {
+		    try {
+		        while (true) {
+		            String str = in.readLine();
+		            if (str.equalsIgnoreCase("END")) {
+		                    break;
+		            }
+		            int a=1+3; //разного рода вычесление
+		            out.println("Я получил Ваши данные, вот то что Вы прислали: "+str);
+		            out.println("Я выполнилась операцию: "+a);
+		        }
+		    } catch (IOException e) {
+		        System.err.println("Соединение № "+ n +". Ошибка при передачи клиенту: " +HostName);
+		    } finally {
+		        try {
+		            socket.close();
+		        } catch (IOException e) {
+		            System.err.println("Соединение № "+ n +". Не получается разорвать связь с клиентом: " +HostName);
+		        }
+		    }
+		    System.out.println("Завершено соединение № "+ n +", клиент: " +HostName+" отключен");
+
+		}
+}
