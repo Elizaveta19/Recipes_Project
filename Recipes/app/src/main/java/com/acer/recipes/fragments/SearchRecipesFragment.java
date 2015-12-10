7cc027ad7bfc9527f4bd05ac48fb21e1c48e0d34
Elizaveta19 <elizaveta-1912@yandex.ru>
2015-12-10 22:41:26 +0200

    Клиент_7
    Пункты из listView удаляются по долгому нажатию, всплывающее контекстное
    меню

--- Recipes/app/src/main/java/com/acer/recipes/fragments/SearchRecipesFragment.java
+++ Recipes/app/src/main/java/com/acer/recipes/fragments/SearchRecipesFragment.java
@@ -10,8 +10,12 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.KeyListener;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -52,9 +56,12 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
 
     private static final int LAYOUT = R.layout.activity_search_recipes;
 
+    public static final int C_MENU_DELETE = 201;
+
     private View view;
     Button sa_searchButton;
     Button sa_helpButton;
+    Button sa_clearButton;
     EditText sa_editText;
     DbOpenHelper dbHelper;
 
@@ -65,6 +72,7 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
     ListView productsListView;
     ArrayList<String> productsArrayList = new ArrayList<>();
     ArrayAdapter<String> adapter;
+    static String itemAtPosition = new String();
 
     ArrayList<Integer> keysList = new ArrayList<>();
 
@@ -78,6 +86,7 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
         sa_editText = (EditText) view.findViewById(R.id.et_product);
         sa_searchButton = (Button) view.findViewById(R.id.sa_searchButton);
         sa_helpButton = (Button) view.findViewById(R.id.sa_helpButton);
+        sa_clearButton = (Button) view.findViewById(R.id.sa_clearButton);
 
         //для вспомогательного списка для выбора продуктов
         productsHelperListView = (ListView) view.findViewById(R.id.products_helper_listView);
@@ -89,9 +98,12 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
         productsListView = (ListView) view.findViewById(R.id.products_listView);
         adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, productsArrayList);
         productsListView.setAdapter(adapter);
+        //productsListView.setOnItemLongClickListener(this);
+        registerForContextMenu(productsListView);
 
         sa_helpButton.setOnClickListener(this);
         sa_searchButton.setOnClickListener(this);
+        sa_clearButton.setOnClickListener(this);
 
         return view;
     }
@@ -101,12 +113,18 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id.sa_helpButton: {
+                productsHelperArrayList.clear();
                 ArrayList<Product> productsArrayList_1 = dbHelper.getProducts(sa_editText.getText().toString().trim());
                 for(Product pr : productsArrayList_1)
                 {
                     productsHelperArrayList.add(pr.getName());
                     helperAdapter.notifyDataSetChanged();
                 }
+                break;
+            }
+            case R.id.sa_clearButton: {
+                productsArrayList.clear();
+                adapter.notifyDataSetChanged();
                 break;
             }
             case R.id.sa_searchButton: {
@@ -131,6 +149,28 @@ public class SearchRecipesFragment extends Fragment implements View.OnClickListe
 
         productsHelperArrayList.clear();
         helperAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        AdapterView.AdapterContextMenuInfo acmi = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        itemAtPosition = (String) productsListView.getItemAtPosition(acmi.position);
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(Menu.NONE, C_MENU_DELETE, menu.NONE, "Удалить");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        switch (item.getItemId()){
+            case C_MENU_DELETE:
+                productsArrayList.remove(itemAtPosition);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+
+        return super.onContextItemSelected(item);
     }
 
     public static SearchRecipesFragment getFragment()
